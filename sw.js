a40da1d20029e1b8e1942211c7ade21b66ce536d
10xOnkar <minus20nkar@gmail.com>
2026-07-20 12:20:29 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "zero1-v9";
+const CACHE = "zero1-v10";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
